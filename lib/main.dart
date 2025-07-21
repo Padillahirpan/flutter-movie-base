e8b5f38ce3852159252206c364ddefb31db31ffb
Padillahirpan <padillahirpan8@gmail.com>
2025-07-21 17:56:34 +0700

    feat: update main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,28 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/firebase_options.dart';
+import 'package:movie_app/presentation/pages/login_page/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(const MyApp());
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Movie App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const LoginPage(),
+      // home: const MyHomePage(title: 'Flutter Movie App Home Page'),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
