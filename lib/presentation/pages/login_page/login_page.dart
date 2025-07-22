993d175ba26ab6df3a8bea7a0e1b574a0cc81b4b
Padillahirpan <padillahirpan8@gmail.com>
2025-07-22 15:10:41 +0700

    feat: update login and main page

--- lib/presentation/pages/login_page/login_page.dart
+++ lib/presentation/pages/login_page/login_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movie_app/domain/usecases/login/login.dart';
 import 'package:movie_app/presentation/pages/main_page/main_page.dart';
+import 'package:movie_app/presentation/providers/router/router_provider.dart';
+import 'package:movie_app/presentation/providers/user_data/user_data_provider.dart';
 import 'package:movie_app/presentation/providers/usercases/login_provider.dart';
 
 class LoginPage extends ConsumerWidget {
@@ -9,29 +11,55 @@ class LoginPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // var user = ref.watch(userDataProvider);
+
+    // var isLoading = user.isLoading;
+    ref.listen(userDataProvider, (previous, next) {
+      if (next is AsyncData) {
+        if (next.value != null) {
+          ref.read(routerProvider).goNamed('main');
+        } else if (next is AsyncError) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text(next.error.toString())));
+        }
+      }
+    });
+
     return Scaffold(
       appBar: AppBar(title: const Text('Login Page')),
       body: Center(
+        // child: Column(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: [
+        //     if (isLoading) const CircularProgressIndicator(),
+        //     if (!isLoading) const Text('Please login to continue'),
+        //   ],
+        // ),
         child: ElevatedButton(
           onPressed: () {
-            Login login = ref.watch(loginProvider);
+            ref
+                .read(userDataProvider.notifier)
+                .login(email: 'test2@test.com', password: '123456');
 
-            login(
-              LoginParams(email: "test2@test.com", password: "123456"),
-            ).then((result) {
-              if (result.isSuccess) {
-                Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(
-                    builder: (context) => MainPage(user: result.resultValue!),
-                  ),
-                );
-              } else {
-                // Handle login failure
-                ScaffoldMessenger.of(
-                  context,
-                ).showSnackBar(SnackBar(content: Text(result.errorMessage!)));
-              }
-            });
+            // Login login = ref.watch(loginProvider);
+
+            // login(
+            //   LoginParams(email: "test2@test.com", password: "123456"),
+            // ).then((result) {
+            //   if (result.isSuccess) {
+            //     Navigator.of(context).pushReplacement(
+            //       MaterialPageRoute(
+            //         builder: (context) => MainPage(user: result.resultValue!),
+            //       ),
+            //     );
+            //   } else {
+            //     // Handle login failure
+            //     ScaffoldMessenger.of(
+            //       context,
+            //     ).showSnackBar(SnackBar(content: Text(result.errorMessage!)));
+            //   }
+            // });
           },
           child: const Text('Login'),
         ),
--- lib/presentation/pages/main_page/main_page.dart
+++ lib/presentation/pages/main_page/main_page.dart
@@ -1,16 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movie_app/domain/entities/user.dart';
+import 'package:movie_app/presentation/providers/user_data/user_data_provider.dart';
 
-class MainPage extends StatelessWidget {
-  final User user;
+class MainPage extends ConsumerStatefulWidget {
+  const MainPage({super.key});
 
-  const MainPage({super.key, required this.user});
+  @override
+  ConsumerState<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends ConsumerState<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    var data = ref.watch(userDataProvider);
+
+    var isLoading = data.isLoading;
+
+    var user_data = ref
+        .watch(userDataProvider)
+        .when(
+          data: (data) => data,
+          error: (error, stackTrace) => '',
+          loading: () => 'Loading',
+        );
+    // return Scaffold(
+    //   appBar: AppBar(title: const Text('Main Page')),
+    //   body: Center(child: Text('Welcome to the Main Page! ${user_data}')),
+    // );
+    if (isLoading) Text('Sabar ini sedang loading kok :)');
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Main Page')),
+      body: Center(
+        child: Column(
+          children: [
+            const CardExample(),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(userDataProvider.notifier).logout();
+              },
+              child: const Text('Logout'),
+            ),
+            Text(
+              'Welcome to the Main Page! ${user_data}',
+              style: const TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardExample extends StatelessWidget {
+  const CardExample({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Main Page')),
-      body: Center(child: Text('Welcome to the Main Page! ${user.name}')),
+    return Center(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const ListTile(
+              leading: Icon(Icons.album),
+              title: Text('Test 123456'),
+              subtitle: Text("Music by Test 123456. Test test test..."),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
