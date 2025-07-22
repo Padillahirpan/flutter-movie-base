24ca6f8d8268d7da22699bfa2cf9dba4b81b64a0
Padillahirpan <padillahirpan8@gmail.com>
2025-07-22 10:35:43 +0700

    feat: add movie repository provider

---
+++ lib/presentation/providers/repositories/movie_repository/movie_repository_provider.dart
@@ -0,0 +1,9 @@
+import 'package:movie_app/data/repositories/movie_repository.dart';
+import 'package:movie_app/data/tmdb/tmdb_movie_repository.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'movie_repository_provider.g.dart';
+
+@riverpod
+MovieRepository movieRepository(MovieRepositoryRef ref) =>
+    TmdbMovieRepository();
